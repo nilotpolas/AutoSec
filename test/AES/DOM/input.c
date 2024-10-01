b3d6a18149bf2fbad43c0c0d6b009c0ee3370939
sujeetK149 <sujeetkamble701@gmail.com>
2024-10-02 01:46:52 +0530

    tests added

--- test/AES/DOM/input.c
+++ test/AES/DOM/input.c
@@ -294,7 +294,7 @@ void G256_inv(int x0, int x1, int *y0, int *y1, int r0, int r1, int r2, int r3, 
     *y1 = (p1ls4 | q1);
 }
 //ap_int<8> G256_newbasis(ap_int<8> in1, ap_int<8> in2, ap_int<8> b[], ap_int<8> *out1, ap_int<8> *out2)
-int G256_newbasis(int in1, int in2, int b[], int *out1, int *out2)
+void G256_newbasis(int in1, int in2, int b[], int *out1, int *out2)
 {
 
     int y, cond, yxorb, negCond, tempy, tempyIntoNegCond;
@@ -470,7 +470,7 @@ int G256_newbasis(int in1, int in2, int b[], int *out1, int *out2)
     *out2 = z_y8;
 }
 
-int sbox(int t0, int t1, int *y0, int *y1, int r0, int r1, int r2, int r3, int r4, int r5, int r6, int r7, int r8,
+void sbox(int t0, int t1, int *y0, int *y1, int r0, int r1, int r2, int r3, int r4, int r5, int r6, int r7, int r8,
          int r9, int r10, int r11, int r12, int r13, int r14, int r15, int r16, int r17,
          int r18, int r19, int r20, int r21, int r22, int r23, int r24, int r25, int r26,
          int r27, int r28, int r29, int r30, int r31, int r32, int r33, int r34, int r35)
--- test/AES/HPC2/input.c
+++ test/AES/HPC2/input.c
@@ -105,7 +105,7 @@ void G4_scl_N(int x0, int x1, int *e0_1, int *e1_1)
     *e1_1 = (p1ls1 | q1);
 }
 // /* scale by N^2 = Omega in GF(2^2), using normal basis (Omega^2,Omega) */
-int G4_scl_N2(int x0, int x1, int *e0_0, int *e1_0)
+void G4_scl_N2(int x0, int x1, int *e0_0, int *e1_0)
 {
     int a0, a1, b0, b1, p1, p2, q1, q2, a0_0, a1_0, p0, q0;
 
@@ -257,7 +257,7 @@ void G16_inv(int x0, int x1, int *y0, int *y1, int r0, int r1, int r2, int r3, i
     *y1 = (p1ls2 | q1);
 }
 
-int G256_inv(int x0, int x1, int *y0, int *y1, int r0, int r1, int r2, int r3, int r4, int r5, int r6, int r7, int r8,
+void G256_inv(int x0, int x1, int *y0, int *y1, int r0, int r1, int r2, int r3, int r4, int r5, int r6, int r7, int r8,
              int r9, int r10, int r11, int r12, int r13, int r14, int r15, int r16, int r17,
              int r18, int r19, int r20, int r21, int r22, int r23, int r24, int r25, int r26,
              int r27, int r28, int r29, int r30, int r31, int r32, int r33, int r34, int r35)
@@ -290,7 +290,7 @@ int G256_inv(int x0, int x1, int *y0, int *y1, int r0, int r1, int r2, int r3, i
     *y1 = (p1ls4 | q1);
 }
 
-int G256_newbasis(int in1, int in2, int b[], int *out1, int *out2)
+void G256_newbasis(int in1, int in2, int b[], int *out1, int *out2)
 {
 
     int y, cond, yxorb, negCond, tempy, tempyIntoNegCond;
@@ -466,7 +466,7 @@ int G256_newbasis(int in1, int in2, int b[], int *out1, int *out2)
     *out2 = z_y8;
 }
 /* find Sbox of n in GF(2^8) mod POLY */
-int sbox(int t0, int t1, int *y0, int *y1, int r0, int r1, int r2, int r3, int r4, int r5, int r6, int r7, int r8,
+void sbox(int t0, int t1, int *y0, int *y1, int r0, int r1, int r2, int r3, int r4, int r5, int r6, int r7, int r8,
          int r9, int r10, int r11, int r12, int r13, int r14, int r15, int r16, int r17,
          int r18, int r19, int r20, int r21, int r22, int r23, int r24, int r25, int r26,
          int r27, int r28, int r29, int r30, int r31, int r32, int r33, int r34, int r35)
